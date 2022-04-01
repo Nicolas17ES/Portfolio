4172bfa22e6dce761ef7d674c4fbe8ae7184ca59
Nicolas17ES <83025990+Nicolas17ES@users.noreply.github.com>
2022-04-01 12:17:17 +0200

    added smooth scroll between home and about

--- src/components/main-page/about/About.jsx
+++ src/components/main-page/about/About.jsx
@@ -1,36 +1,48 @@
 import './About.css'
-import {AiFillPlusCircle} from 'react-icons/ai'
+import { ReactComponent as Vinyl }  from '../../assets/vinyl.svg'
+import { ReactComponent as Road }  from '../../assets/road.svg'
+import { ReactComponent as Laptop }  from '../../assets/laptop.svg'
 import {useRef, useContext, useEffect} from 'react'
+import handleViewport from 'react-in-viewport';
 import FeedbackContext from '../../../context/feedback/FeedbackContext'
 
 
 
+function About(props) {
 
-function About() {
-
-    const {scroll} = useContext(FeedbackContext);
+    const {scroll, dispatch} = useContext(FeedbackContext);
     const scrollTo = useRef();
+    
+   
 
     useEffect(() => {
-        
         if(scroll === true){
             scrollTo.current.scrollIntoView({ behavior: "smooth", block: "center"})
+            dispatch({
+                    type: 'SCROLL_VIEW',
+                    payload: false
+                })
         }
 
     }, [scroll])
 
-    return (
-        <div  className="about-page">
+
+const Block = (props) => {
+    const { inViewport, forwardedRef, enterCount } = props;
+
+    if (inViewport && enterCount === 1) {
+        return (
+          <div ref={forwardedRef}  className="about-page">
             <section className="section-two">
                 <div className="box1 box">ABOUT ME</div>
                 <div className="box2 box"></div>
-                <div className="box3 box"><p>Started my Web Dev journey two years ago as a self-taught student. It quickly became a passion so...</p></div>
+                <div className="box3 box"><Laptop className="laptop about-svg"/></div>
                 <div className="box4 box">WHAT I DO</div>
                 <div className="box5 box"></div>
-                <div ref={scrollTo} className="box6 box"><p>Started my Web Dev journey two years ago as a self-taught student. It quickly became a passion so...</p><AiFillPlusCircle size={18} className="plus-icon"/></div>
+                <div ref={scrollTo} className="box6 box"><Road className="road about-svg"/></div>
                 <div className="box7 box">THINGS I LOVE</div>
                 <div className="box8 box"></div>
-                <div className="box9 box"><p>I am obssesed with creating things, applying creativity and solving problems</p></div>
+                <div className="box9 box"><Vinyl className="vinyl about-svg"/></div>
                 <div className="box10 box"></div>
                 <div className="box11 box"></div>
                 <div className="box12 box"></div>
@@ -41,6 +53,41 @@ function About() {
                 <div className="box19 box"></div>
             </section>
         </div>
+        )
+    }
+    return (
+        
+        <div ref={forwardedRef}  className="about-page">
+            <section className="section-two">
+                <div className="box1 box">ABOUT ME</div>
+                <div className="box2 box"></div>
+                <div className="box3 box"><Laptop className="laptop about-svg"/></div>
+                <div className="box4 box">WHAT I DO</div>
+                <div className="box5 box"></div>
+                <div ref={scrollTo} className="box6 box"><Road className="road about-svg"/></div>
+                <div className="box7 box">THINGS I LOVE</div>
+                <div className="box8 box"></div>
+                <div className="box9 box"><Vinyl className="vinyl about-svg"/></div>
+                <div className="box10 box"></div>
+                <div className="box11 box"></div>
+                <div className="box12 box"></div>
+                <div className="box13 box"></div>
+                <div className="box14 box"></div>
+                <div className="box15 box"></div>
+                <div className="box16 box"></div>
+                <div className="box19 box"></div>
+            </section>
+        </div>
+    );
+};
+const ViewportBlock = handleViewport(Block);
+
+    return (
+        <>
+            <ViewportBlock onEnterViewport={() => console.log('enter')} onLeaveViewport={() => console.log('leave')} />
+            
+        </>
+        
     )
 }
 
